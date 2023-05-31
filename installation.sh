416a9a137160f47215fa44ed952f86e595db290e
Stephane Schittly <st-f@users.noreply.github.com>
2023-05-31 10:04:19 +0100

    Update installation.sh

--- installation.sh
+++ installation.sh
@@ -1,3 +1,6 @@
+cd ~/Applications
+git clone git@github.com:st-f/adb-helper.git
+cd adb-helper
 chmod 755 adb-helper
 echo 'alias adb-helper="sh ~/Applications/adb-helper/adb-helper.sh"' >> ~/.bash_profile
 source ~/.bash_profile
